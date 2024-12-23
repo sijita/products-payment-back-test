07a4f61855915fe957631d9fa41ab2f5cd6f71f4
Simón Jiménez Tamayo <65623835+sijita@users.noreply.github.com>
2024-12-23 10:50:49 -0500

    feat: add blank customers module
    Add relation between customers and transactions

---
+++ src/modules/customers/entities/customer.entity.ts
@@ -0,0 +1,32 @@
+import { Transaction } from 'src/modules/transactions/entities/transaction.entity';
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  OneToMany,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+export class Customer {
+  @PrimaryGeneratedColumn('uuid')
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column()
+  email: string;
+
+  @OneToMany(() => Transaction, (transaction) => transaction.customer)
+  transactions: Transaction[];
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @DeleteDateColumn()
+  deletedAt: Date;
+}
